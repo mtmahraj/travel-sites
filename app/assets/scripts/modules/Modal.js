10e291b2dc9b69dc1ac38291c34aebcab8e2a44e
tahir <mtmahraj@gmail.com>
2020-03-20 16:51:49 -0400

    Added links to social websites.

--- app/assets/scripts/modules/Modal.js
+++ app/assets/scripts/modules/Modal.js
@@ -39,10 +39,10 @@ class Modal {
       </div>
 
       <div class="social-icons">
-        <a href="#" class="social-icons__icon"><img src="assets/images/icons/facebook.svg" alt="Facebook"></a>
-        <a href="#" class="social-icons__icon"><img src="assets/images/icons/twitter.svg" alt="Twitter"></a>
-        <a href="#" class="social-icons__icon"><img src="assets/images/icons/instagram.svg" alt="Instagram"></a>
-        <a href="#" class="social-icons__icon"><img src="assets/images/icons/youtube.svg" alt="YouTube"></a>
+        <a href="http://www.facebook.com" class="social-icons__icon"><img src="assets/images/icons/facebook.svg" alt="Facebook"></a>
+        <a href="http://www.twitter.com" class="social-icons__icon"><img src="assets/images/icons/twitter.svg" alt="Twitter"></a>
+        <a href="http://www.instagram.com" class="social-icons__icon"><img src="assets/images/icons/instagram.svg" alt="Instagram"></a>
+        <a href="http://www.youtube.com" class="social-icons__icon"><img src="assets/images/icons/youtube.svg" alt="YouTube"></a>
       </div>
     </div>
     <div class="modal__close">X</div>
